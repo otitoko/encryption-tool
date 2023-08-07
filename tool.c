515dcfb7444d8dc08ccc7e14b6c63deb175b2ca9
otitoko <tbkopera@proton.me>
2023-08-07 18:51:35 +0100

    next steps

--- tool.c
+++ tool.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/evp.h>
+#define MAX_LIMIT 1024
+int operation;
+int shift;
+
+void configureCipher(){
+	printf("Would you like to encrypt or decrypt?\n");
+	printf("1. Encrypt\n");
+	printf("2. Decrypt\n");
+
+	scanf("%d",&operation);
+
+	printf("How much shift?\n");
+	scanf("%d",&shift);
+}
+
+int encryption(int shift){
+	printf("Enter the phrase you wish to encrypt: \n");
+	char unencryptedPhrase[MAX_LIMIT]
+	fgets(unencryptedPhrase, MAX_LIMIT, stdin);
+	unencryptedPhrase[strcspn(unencryptedPhrase, "\n")] = 0;
+}
 
 int main(){
-	
+	configureCipher();
 }
