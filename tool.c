eb99c7bc78dd420b7d683fc22cfbedd2391f5e26
otitoko <tbkopera@proton.me>
2023-08-24 17:00:36 +0100

    return t form

--- tool.c
+++ tool.c
@@ -4,6 +4,7 @@
 #define MAX_LIMIT 1024
 int operation;
 int shift;
+char *unencryptedPhrase;
 
 void configureCipher(){
 	printf("Would you like to encrypt or decrypt?\n");
@@ -14,15 +15,31 @@ void configureCipher(){
 
 	printf("How much shift?\n");
 	scanf("%d",&shift);
-}
 
-int encryption(int shift){
+	unencryptedPhrase = (char *)malloc(MAX_LIMIT * sizeof(char));
+	if (unencryptedPhrase == NULL){
+		printf("Mem Alloc failed.\n");
+		exit(1);
+	}
+
+
 	printf("Enter the phrase you wish to encrypt: \n");
-	char unencryptedPhrase[MAX_LIMIT]
+	getchar();
 	fgets(unencryptedPhrase, MAX_LIMIT, stdin);
 	unencryptedPhrase[strcspn(unencryptedPhrase, "\n")] = 0;
 }
 
+void encryption(char *unencryptedPhrase, int shift){
+	//how to do this??
+	//at a char level: loop through each char and put it forward by shift amount of spaces
+	//if too far forward where it is no longer a character, loop it back.
+	
+}
+
 int main(){
 	configureCipher();
+	printf("The unencrypted phrase is : %s",unencryptedPhrase);
+
+	free(unencryptedPhrase);
+	return 0;
 }
