e5cd550e40d9114faf852ec0c2968e0bac1582d7
otitoko <tbkopera@proton.me>
2023-08-25 13:32:30 +0100

    encryption and decryption

--- tool.c
+++ tool.c
@@ -10,6 +10,7 @@ void configureCipher(){
 	printf("Would you like to encrypt or decrypt?\n");
 	printf("1. Encrypt\n");
 	printf("2. Decrypt\n");
+	printf("3. Exit\n");
 
 	scanf("%d",&operation);
 
@@ -33,12 +34,30 @@ void encryption(char *unencryptedPhrase, int shift){
 	//how to do this??
 	//at a char level: loop through each char and put it forward by shift amount of spaces
 	//if too far forward where it is no longer a character, loop it back.
-	
+	for(int i = 0;i < strlen(unencryptedPhrase);i++){
+		int ascii = unencryptedPhrase[i];
+		ascii+=shift;
+		unencryptedPhrase[i] = ascii;
+	}	
+}
+
+void decryption(char *unencryptedPhrase, int shift){
+	for(int i = 0; i < strlen(unencryptedPhrase); i++){
+		int ascii = unencryptedPhrase[i];
+		ascii-=shift;
+		unencryptedPhrase[i] = ascii;
+	}
 }
 
 int main(){
 	configureCipher();
-	printf("The unencrypted phrase is : %s",unencryptedPhrase);
+	printf("The unencrypted phrase is : %s\n",unencryptedPhrase);
+
+	encryption(unencryptedPhrase, shift);
+	printf("The encrypted phrase is : %s\n",unencryptedPhrase);
+
+	decryption(unencryptedPhrase, shift);
+	printf("The encrypted phrase is : %s\n",unencryptedPhrase);
 
 	free(unencryptedPhrase);
 	return 0;
